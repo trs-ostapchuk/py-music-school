eb8d607338200d6594d5ef18db2549a9794d68fd
Taras <tarasostapchuk27@gmail.com>
2025-12-28 10:18:27 +0100

    Modify serializer

--- musician/serializer.py
+++ musician/serializer.py
@@ -3,13 +3,15 @@ from rest_framework import serializers
 
 
 class MusicianSerializer(serializers.ModelSerializer):
-    model = Musician
-    fields = (
-        "id",
-        "first_name",
-        "last_name",
-        "instrument",
-        "age",
-        "date_of_applying",
-        "is_adult"
-    )
+    class Meta:
+
+        model = Musician
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "instrument",
+            "age",
+            "date_of_applying",
+            "is_adult"
+        )
