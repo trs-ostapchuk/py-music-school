94c232ee86a57c8b9c54d591c016cecfb56ce4cc
Taras <tarasostapchuk27@gmail.com>
2025-12-28 10:29:47 +0100

    Fix serializers

--- musician/serializers.py
+++ musician/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 
 class MusicianSerializer(serializers.ModelSerializer):
+    is_adult = serializers.ReadOnlyField()
     class Meta:
 
         model = Musician
